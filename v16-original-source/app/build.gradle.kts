9ac679d6fd1577ea8ec7d9ab40d004e31632101e
moumouhikaram-cell <moumouhi.karam@gmail.com>
2026-09-03 01:16:58 +0200

    chore(android): bump A2 alpha version

--- v16-original-source/app/build.gradle.kts
+++ v16-original-source/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.lapauseclub.manager"
         minSdk = 26
         targetSdk = 36
-        versionCode = 20
-        versionName = "1.7.0-alpha1"
+        versionCode = 21
+        versionName = "1.7.1-alpha2"
     }
 
     buildTypes {
