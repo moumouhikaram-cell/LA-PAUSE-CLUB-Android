21c260a45b93fed6ab7d9dc655c71a57de9609ef
moumouhikaram-cell <moumouhi.karam@gmail.com>
2026-09-03 19:55:12 +0200

    release(android): bump client final to 2.2.0

--- v16-original-source/app/build.gradle.kts
+++ v16-original-source/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.lapauseclub.manager"
         minSdk = 26
         targetSdk = 36
-        versionCode = 26
-        versionName = "2.1.1-beta1"
+        versionCode = 27
+        versionName = "2.2.0"
     }
 
     buildTypes {
