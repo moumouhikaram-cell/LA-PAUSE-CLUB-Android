a7aee0023a002329695baa78eb44efb8d54913cc
moumouhikaram-cell <moumouhi.karam@gmail.com>
2026-09-04 16:13:29 +0200

    chore(v240): bump Android package to LA PAUSE OS 2.4.0

--- v16-original-source/app/build.gradle.kts
+++ v16-original-source/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.lapauseclub.manager"
         minSdk = 26
         targetSdk = 36
-        versionCode = 28
-        versionName = "2.3.0"
+        versionCode = 29
+        versionName = "2.4.0"
     }
 
     buildTypes {
